8511b99e3dc1e681471366e51815ca6da18f8b0c
FunkyMuse <crazyxylene@gmail.com>
2021-04-12 22:24:08 +0200

    refactoring small stuff

--- app/src/main/java/com/funkymuse/aurora/latestBooks/LatestBooksUi.kt
+++ app/src/main/java/com/funkymuse/aurora/latestBooks/LatestBooksUi.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
@@ -12,7 +11,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltNavGraphViewModel
 import androidx.navigation.NavBackStackEntry
-import com.crazylegend.kotlinextensions.log.debug
 import com.crazylegend.retrofit.retrofitResult.RetrofitResult
 import com.crazylegend.retrofit.retrofitResult.handle
 import com.crazylegend.retrofit.retryOnConnectedToInternet
@@ -94,10 +92,7 @@ fun ShowBooks(
     list: List<Book>,
     onBookClicked: (Book) -> Unit,
 ) {
-    val scrollState = rememberLazyListState()
-    scrollState.debug { "FIRST VISIBLE index ${scrollState.firstVisibleItemIndex}" }
     LazyColumn(
-        state = scrollState,
         modifier = modifier,
         contentPadding = LocalWindowInsets.current.systemBars.toPaddingValues()
     ) {
