6007a730efac4026d40093778f56f74ae314ac07
FunkyMuse <crazyxylene@gmail.com>
2021-06-30 13:28:10 +0200

    add a extension function for the future

--- paging/src/main/java/com/funkymuse/aurora/paging/Extensions.kt
+++ paging/src/main/java/com/funkymuse/aurora/paging/Extensions.kt
@@ -1,7 +1,10 @@
 package com.funkymuse.aurora.paging
 
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.paging.PagingSource
 import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
 
 /**
  * Created by funkymuse on 6/25/21 to long live and prosper !
@@ -14,3 +17,10 @@ val LazyPagingItems<*>.prependState get() = loadState.prepend
 
 fun <T : Any> canNotLoadMoreContent(): PagingSource.LoadResult.Page<Int, T> =
         PagingSource.LoadResult.Page(emptyList(), null, null)
+
+
+/*
+val lifecycle = LocalLifecycleOwner.current.lifecycle
+val favorites = remember(pagingData, lifecycle) {
+        pagingData.flowWithLifecycle(lifecycle)
+}.collectAsLazyPagingItems()*/
